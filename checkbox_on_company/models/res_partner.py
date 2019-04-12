da4ebccec5950bb2e0707db40641551eccb9a8c9
Jana Jezova <jje@odoo.com>
2019-04-12 16:33:55 +0200

    deleting old folder checkbox_on_company

--- checkbox_on_company/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    has_limit = fields.Boolean(string="Active Outstanding Payment Limit", default=False)
-
