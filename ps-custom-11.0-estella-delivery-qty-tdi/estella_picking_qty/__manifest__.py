c9ffc686692eeadc71b1c16223a7c2f3bdc3518e
Jana Jezova <jje@odoo.com>
2019-04-18 14:21:32 +0200

    Old development from saas done by TDI to be tested by customer.

---
+++ ps-custom-11.0-estella-delivery-qty-tdi/estella_picking_qty/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Estella Stock Quantity Customization",
+    "description": """
+Estella Stock Quantity Customization
+    """,
+    "depends": ["sale_stock"],
+    "data": [
+    	"models/picking.xml",
+        "views/picking.xml",
+        "actions/automated_action.xml",
+    ]
+}
---
+++ ps-custom-11.0-estella-delivery-qty-tdi/estella_sale_stock/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+{
+    "name": "Estella Sale Stock Customization",
+    "description": """
+Estella Sale Stock Customization Customization..
+    """,
+    "depends": ["sale_stock"],
+    "data": [
+        "data/server_actions/sale_order.xml"
+    ]
+}
