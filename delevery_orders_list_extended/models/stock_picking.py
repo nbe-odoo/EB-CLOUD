fe710c69538a09bce3c763e5ddf6441ebaadce17
mar <arthur@oos.be>
2019-05-03 12:16:44 +0200

    add module to staging

--- delevery_orders_list_extended/models/stock_picking.py
+++ delevery_orders_list_extended/models/stock_picking.py
@@ -11,10 +11,10 @@ class StockPicking(models.Model):
 
     @api.depends('move_ids_without_package')
     def _compute_sum_initial_demand(self):
-        delevery_product = self.env['delivery.carrier'].search([]).mapped('product_id')
+        delivery_product = self.env['delivery.carrier'].search([]).mapped('product_id')
         for rec in self:
             for move_id in rec.move_ids_without_package:
-                if move_id.product_id not in delevery_product:
+                if move_id.product_id not in delivery_product:
                     rec.percentage_reserved += move_id.reserved_availability
                     rec.sum_initial_demand += move_id.product_uom_qty
             if rec.sum_initial_demand:
