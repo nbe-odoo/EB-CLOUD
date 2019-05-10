6192e459bf634a0836e7fecd6d33755ddc4863a4
Jana Jezova <jje@odoo.com>
2019-05-10 14:32:21 +0200

    recomputing

--- sale_module_reporting/models/sale_order_line.py
+++ sale_module_reporting/models/sale_order_line.py
@@ -4,14 +4,14 @@ from odoo.exceptions import UserError
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    untaxed_amount_reserved = fields.Float(compute='_compute_untaxed_reserved', string='Untaxed Amount Reserved', store=True)
-    untaxed_amount_undelivered = fields.Float(compute='_compute_untaxed_undelivered', string='Untaxed Undelivered', store=True)
-    quantity_reserved = fields.Float(compute='_compute_qty_reserved', string='Quantity Reserved', store=True)
-    quantity_undelivered = fields.Float(compute='_compute_qty_undelivered', string='Quantity Undelivered', store=True)
+    untaxed_amount_reserved = fields.Float(compute='_compute_untaxed_reserved2', string='Untaxed Amount Reserved', store=True)
+    untaxed_amount_undelivered = fields.Float(compute='_compute_untaxed_undelivered2', string='Untaxed Undelivered', store=True)
+    quantity_reserved = fields.Float(compute='_compute_qty_reserved2', string='Quantity Reserved', store=True)
+    quantity_undelivered = fields.Float(compute='_compute_qty_undelivered2', string='Quantity Undelivered', store=True)
 
     @api.multi
     @api.depends('move_ids', 'move_ids.state', 'price_unit', 'move_ids.reserved_availability')
-    def _compute_untaxed_reserved(self):
+    def _compute_untaxed_reserved2(self):
         for so_line in self:
             for delivery in so_line.move_ids:
                 if so_line.move_ids and delivery.state not in ['cancel', 'done']:
@@ -21,7 +21,7 @@ class SaleOrderLine(models.Model):
 
     @api.multi
     @api.depends('move_ids', 'move_ids.state', 'price_unit', 'move_ids.product_uom_qty', 'move_ids.quantity_done')
-    def _compute_untaxed_undelivered(self):
+    def _compute_untaxed_undelivered2(self):
         for so_line in self:
             for delivery in so_line.move_ids:
                 if so_line.move_ids and delivery.state not in ['cancel', 'done']:
@@ -31,7 +31,7 @@ class SaleOrderLine(models.Model):
 
     @api.multi
     @api.depends('move_ids', 'move_ids.state', 'move_ids.reserved_availability')
-    def _compute_qty_reserved(self):
+    def _compute_qty_reserved2(self):
         for so_line in self:
             for delivery in so_line.move_ids:
                 if so_line.move_ids and delivery.state not in ['cancel', 'done']:
@@ -41,7 +41,7 @@ class SaleOrderLine(models.Model):
 
     @api.multi
     @api.depends('move_ids', 'move_ids.state', 'move_ids.product_uom_qty', 'move_ids.quantity_done')
-    def _compute_qty_undelivered(self):
+    def _compute_qty_undelivered2(self):
         for so_line in self:
             for delivery in so_line.move_ids:
                 if so_line.move_ids and delivery.state not in ['cancel', 'done']:
