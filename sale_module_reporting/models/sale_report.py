3dba8b7c81e1d4f9587aa7d21538603142a5c4d5
Jana Jezova <jje@odoo.com>
2019-05-10 14:38:24 +0200

    New fields instead of recomputing everything.

--- sale_module_reporting/models/sale_report.py
+++ sale_module_reporting/models/sale_report.py
@@ -4,18 +4,18 @@ from odoo.exceptions import UserError
 class SaleReport(models.Model):
     _inherit = 'sale.report'
 
-    untaxed_amount_reserved = fields.Float(string='Untaxed Amount Reserved', readonly=True)
-    untaxed_amount_undelivered = fields.Float(string='Untaxed Amount Undelivered', readonly=True)
-    quantity_reserved = fields.Float(string='Quantity Reserved', readonly=True)
-    quantity_undelivered = fields.Float(string='Quantity Undelivered', readonly=True)
+    untaxed_amount_reserved2 = fields.Float(string='Untaxed Amount Reserved', readonly=True)
+    untaxed_amount_undelivered2 = fields.Float(string='Untaxed Amount Undelivered', readonly=True)
+    quantity_reserved2 = fields.Float(string='Quantity Reserved', readonly=True)
+    quantity_undelivered2 = fields.Float(string='Quantity Undelivered', readonly=True)
     effective_date = fields.Date(string='Effective Date', readonly=True)
     commitment_date = fields.Date(string='Commitment Date', readonly=True)
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
-        fields['untaxed_amount_reserved'] = ', sum(l.untaxed_amount_reserved) as untaxed_amount_reserved'
-        fields['untaxed_amount_undelivered'] = ', sum(l.untaxed_amount_undelivered) as untaxed_amount_undelivered'
-        fields['quantity_reserved'] = ', sum(l.quantity_reserved) as quantity_reserved'
-        fields['quantity_undelivered'] = ', sum(l.quantity_undelivered) as quantity_undelivered'
+        fields['untaxed_amount_reserved2'] = ', sum(l.untaxed_amount_reserved2) as untaxed_amount_reserved2'
+        fields['untaxed_amount_undelivered2'] = ', sum(l.untaxed_amount_undelivered2) as untaxed_amount_undelivered2'
+        fields['quantity_reserved2'] = ', sum(l.quantity_reserved2) as quantity_reserved2'
+        fields['quantity_undelivered2'] = ', sum(l.quantity_undelivered2) as quantity_undelivered2'
         fields['effective_date'] = ', s.effective_date'
         fields['commitment_date'] = ', s.commitment_date'
 
