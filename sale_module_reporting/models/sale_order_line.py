e16ec3e0f755fb66de93eff3b48f402fc53fa5c9
Jana Jezova <jje@odoo.com>
2019-05-13 10:28:43 +0200

    Computing quantity undelivered directly in the sale order.

--- sale_module_reporting/models/sale_order_line.py
+++ sale_module_reporting/models/sale_order_line.py
@@ -4,10 +4,10 @@ from odoo.exceptions import UserError
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    untaxed_amount_reserved2 = fields.Float(compute='_compute_untaxed_reserved', string='Untaxed Amount Reserved', store=True)
-    untaxed_amount_undelivered2 = fields.Float(compute='_compute_untaxed_undelivered', string='Untaxed Undelivered', store=True)
-    quantity_reserved2 = fields.Float(compute='_compute_qty_reserved', string='Quantity Reserved', store=True)
-    quantity_undelivered2 = fields.Float(compute='_compute_qty_undelivered', string='Quantity Undelivered', store=True)
+    untaxed_amount_reserved1 = fields.Float(compute='_compute_untaxed_reserved', string='Untaxed Amount Reserved', store=True)
+    untaxed_amount_undelivered1 = fields.Float(compute='_compute_untaxed_undelivered', string='Untaxed Undelivered', store=True)
+    quantity_reserved1 = fields.Float(compute='_compute_qty_reserved', string='Quantity Reserved', store=True)
+    quantity_undelivered1 = fields.Float(compute='_compute_qty_undelivered', string='Quantity Undelivered', store=True)
 
     @api.multi
     @api.depends('move_ids', 'move_ids.state', 'price_unit', 'move_ids.reserved_availability')
@@ -15,19 +15,18 @@ class SaleOrderLine(models.Model):
         for so_line in self:
             for delivery in so_line.move_ids:
                 if so_line.move_ids and delivery.state not in ['cancel', 'done']:
-                    so_line.untaxed_amount_reserved2 = delivery.reserved_availability * so_line.price_unit
+                    so_line.untaxed_amount_reserved1 = delivery.reserved_availability * so_line.price_unit
                 elif so_line.move_ids and delivery.state in ['cancel', 'done']:
-                    so_line.untaxed_amount_reserved2 = 0
+                    so_line.untaxed_amount_reserved1 = 0
 
     @api.multi
     @api.depends('move_ids', 'move_ids.state', 'price_unit', 'move_ids.product_uom_qty', 'move_ids.quantity_done')
     def _compute_untaxed_undelivered(self):
         for so_line in self:
-            for delivery in so_line.move_ids:
-                if so_line.move_ids and delivery.state not in ['cancel', 'done']:
-                    so_line.untaxed_amount_undelivered2 = (delivery.product_uom_qty - delivery.quantity_done) * so_line.price_unit
-                elif so_line.move_ids and delivery.state in ['cancel', 'done']:
-                    so_line.untaxed_amount_undelivered2 = 0
+            if so_line.move_ids and so_line.move_ids[0].state not in ['cancel']:
+                so_line.untaxed_amount_undelivered1 = (so_line.product_uom_qty - so_line.qty_delivered) * so_line.price_unit
+            elif so_line.move_ids and so_line.move_ids[0].state in ['cancel']:
+                so_line.untaxed_amount_undelivered1 = 0
 
     @api.multi
     @api.depends('move_ids', 'move_ids.state', 'move_ids.reserved_availability')
@@ -35,16 +34,15 @@ class SaleOrderLine(models.Model):
         for so_line in self:
             for delivery in so_line.move_ids:
                 if so_line.move_ids and delivery.state not in ['cancel', 'done']:
-                    so_line.quantity_reserved2 = delivery.reserved_availability
+                    so_line.quantity_reserved1 = delivery.reserved_availability
                 elif so_line.move_ids and delivery.state in ['cancel', 'done']:
-                    so_line.quantity_reserved2 = 0
+                    so_line.quantity_reserved1 = 0
 
     @api.multi
     @api.depends('move_ids', 'move_ids.state', 'move_ids.product_uom_qty', 'move_ids.quantity_done')
     def _compute_qty_undelivered(self):
         for so_line in self:
-            for delivery in so_line.move_ids:
-                if so_line.move_ids and delivery.state not in ['cancel', 'done']:
-                    so_line.quantity_undelivered2 = delivery.product_uom_qty - delivery.quantity_done
-                elif so_line.move_ids and delivery.state in ['cancel', 'done']:
-                    so_line.quantity_undelivered2 = 0
+            if so_line.move_ids and so_line.move_ids[0].state not in ['cancel']:
+                so_line.quantity_undelivered1 = so_line.product_uom_qty - so_line.qty_delivered
+            elif so_line.move_ids and so_line.move_ids[0].state in ['cancel']:
+                so_line.quantity_undelivered1 = 0
--- sale_module_reporting/models/sale_report.py
+++ sale_module_reporting/models/sale_report.py
@@ -4,18 +4,18 @@ from odoo.exceptions import UserError
 class SaleReport(models.Model):
     _inherit = 'sale.report'
 
-    untaxed_amount_reserved2 = fields.Float(string='Untaxed Amount Reserved', readonly=True)
-    untaxed_amount_undelivered2 = fields.Float(string='Untaxed Amount Undelivered', readonly=True)
-    quantity_reserved2 = fields.Float(string='Quantity Reserved', readonly=True)
-    quantity_undelivered2 = fields.Float(string='Quantity Undelivered', readonly=True)
+    untaxed_amount_reserved1 = fields.Float(string='Untaxed Amount Reserved', readonly=True)
+    untaxed_amount_undelivered1 = fields.Float(string='Untaxed Amount Undelivered', readonly=True)
+    quantity_reserved1 = fields.Float(string='Quantity Reserved', readonly=True)
+    quantity_undelivered1 = fields.Float(string='Quantity Undelivered', readonly=True)
     effective_date = fields.Date(string='Effective Date', readonly=True)
     commitment_date = fields.Date(string='Commitment Date', readonly=True)
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
-        fields['untaxed_amount_reserved2'] = ', sum(l.untaxed_amount_reserved2) as untaxed_amount_reserved2'
-        fields['untaxed_amount_undelivered2'] = ', sum(l.untaxed_amount_undelivered2) as untaxed_amount_undelivered2'
-        fields['quantity_reserved2'] = ', sum(l.quantity_reserved2) as quantity_reserved2'
-        fields['quantity_undelivered2'] = ', sum(l.quantity_undelivered2) as quantity_undelivered2'
+        fields['untaxed_amount_reserved1'] = ', sum(l.untaxed_amount_reserved1) as untaxed_amount_reserved1'
+        fields['untaxed_amount_undelivered1'] = ', sum(l.untaxed_amount_undelivered1) as untaxed_amount_undelivered1'
+        fields['quantity_reserved1'] = ', sum(l.quantity_reserved1) as quantity_reserved1'
+        fields['quantity_undelivered1'] = ', sum(l.quantity_undelivered1) as quantity_undelivered1'
         fields['effective_date'] = ', s.effective_date'
         fields['commitment_date'] = ', s.commitment_date'
 
