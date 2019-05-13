3d2d2b694c9574ccd57242a1408797dadac20f91
Jana Jezova <jje@odoo.com>
2019-05-13 15:51:01 +0200

    Set the rate according to the creation date of the order (not confirmation date). Take into account discount.

--- sale_module_reporting/models/sale_report.py
+++ sale_module_reporting/models/sale_report.py
@@ -4,18 +4,18 @@ from odoo.exceptions import UserError
 class SaleReport(models.Model):
     _inherit = 'sale.report'
 
-    untaxed_amount_reserved4 = fields.Float(string='Untaxed Amount Reserved', readonly=True)
-    untaxed_amount_undelivered4 = fields.Float(string='Untaxed Amount Undelivered', readonly=True)
-    quantity_reserved4 = fields.Float(string='Quantity Reserved', readonly=True)
-    quantity_undelivered4 = fields.Float(string='Quantity Undelivered', readonly=True)
+    untaxed_amount_reserved9 = fields.Float(string='Untaxed Amount Reserved', readonly=True)
+    untaxed_amount_undelivered9 = fields.Float(string='Untaxed Amount Undelivered', readonly=True)
+    quantity_reserved9 = fields.Float(string='Quantity Reserved', readonly=True)
+    quantity_undelivered9 = fields.Float(string='Quantity Undelivered', readonly=True)
     effective_date = fields.Date(string='Effective Date', readonly=True)
     commitment_date = fields.Date(string='Commitment Date', readonly=True)
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
-        fields['untaxed_amount_reserved4'] = ', sum(l.untaxed_amount_reserved4) as untaxed_amount_reserved4'
-        fields['untaxed_amount_undelivered4'] = ', sum(l.untaxed_amount_undelivered4) as untaxed_amount_undelivered4'
-        fields['quantity_reserved4'] = ', sum(l.quantity_reserved4) as quantity_reserved4'
-        fields['quantity_undelivered4'] = ', sum(l.quantity_undelivered4) as quantity_undelivered4'
+        fields['untaxed_amount_reserved9'] = ', sum(l.untaxed_amount_reserved9) as untaxed_amount_reserved9'
+        fields['untaxed_amount_undelivered9'] = ', sum(l.untaxed_amount_undelivered9) as untaxed_amount_undelivered9'
+        fields['quantity_reserved9'] = ', sum(l.quantity_reserved9) as quantity_reserved9'
+        fields['quantity_undelivered9'] = ', sum(l.quantity_undelivered9) as quantity_undelivered9'
         fields['effective_date'] = ', s.effective_date'
         fields['commitment_date'] = ', s.commitment_date'
 
