c72c0e667c30c915a7635ca69b34d4feea8dba93
Andreatta Sébastien <sea@odoo.com>
2019-10-21 09:20:13 +0200

    Add monkey patch for test_invoice_currency_onchange

--- default_pricelist_credit/models/account_invoice_pricelist.py
+++ default_pricelist_credit/models/account_invoice_pricelist.py
@@ -11,5 +11,20 @@ class AccountInvoice(models.Model):
 
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
+        res = super(AccountInvoice, self)._onchange_partner_id()
+        
         self.pricelist_id = self.partner_id.property_product_pricelist.id
         self.currency_id = self.partner_id.property_product_pricelist.currency_id
+        
+        return res
+
+
+class AccountInvoiceLIne(models.Model):
+    _inherit = 'account.invoice.line'
+
+    @api.onchange('uom_id')
+    def _onchange_uom_id(self):
+        res = super(AccountInvoiceLIne, self)._onchange_uom_id()
+        if self.product_id and self.invoice_id.pricelist_id:
+            self.price_unit = self.invoice_id.pricelist_id.get_product_price(self.product_id, 1.0, self.invoice_id.partner_id)
+        return res
