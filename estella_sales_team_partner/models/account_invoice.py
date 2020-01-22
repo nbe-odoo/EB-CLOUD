f04335099006f86250b918fd8b5dd8c1db805c29
Maurin3 <maurin3_1994@msn.com>
2020-01-22 20:27:09 +0100

    [DEV] estella_sales_team_partner: Dev of the module

---
+++ estella_sales_team_partner/models/account_invoice.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, api, models, _
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        if self.partner_id.team_id:
+            self.team_id = self.partner_id.team_id
