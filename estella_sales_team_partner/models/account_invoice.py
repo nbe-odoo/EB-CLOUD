d172fed2a952cfb6ec7b944def5d9b299700d3fc
Maurin3 <maurin3_1994@msn.com>
2020-01-24 10:31:14 +0100

    [FIX] estella_sales_team_partner: Fix module
    * Removed the account.invoice form view since team_id is already in standard views
    * Changed the name of the onchange function for account.invoice and made it inherit the standard one

--- estella_sales_team_partner/models/account_invoice.py
+++ estella_sales_team_partner/models/account_invoice.py
@@ -5,7 +5,8 @@ from odoo import fields, api, models, _
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    @api.onchange('partner_id')
-    def onchange_partner_id(self):
+    @api.onchange('partner_id', 'company_id')
+    def _onchange_partner_id(self):
+        import ipdb; ipdb.set_trace()
         if self.partner_id.team_id:
             self.team_id = self.partner_id.team_id
